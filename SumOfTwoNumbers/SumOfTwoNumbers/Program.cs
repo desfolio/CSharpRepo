0a966d1cc2699b6a454afbe20268e7b35279629b
Desmond.Williams <Desmond.Williams@englemartin.com>
2020-05-05 06:53:45 -0400

    Simple addition Console program

---
+++ SumOfTwoNumbers/SumOfTwoNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace SumOfTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Write a C# Sharp program to print the sum of two numbers.
+             */
+
+            
+            Console.WriteLine("Please enter your first number: ");
+            var firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Please enter your second number: ");
+            var secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            var addResult = firstNumber + secondNumber; 
+
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {addResult}");
+        }
+    }
+}
